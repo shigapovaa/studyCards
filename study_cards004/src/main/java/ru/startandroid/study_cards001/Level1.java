1a2368dba4176fdc67548835747aadf59b63c599
Mark Zhukov <MarkZhukov@bitbucket.org>
2016-11-20 17:38:30 +0300

    deleted errors

---
+++ study_cards004/src/main/java/ru/startandroid/study_cards001/Level1.java
@@ -0,0 +1,56 @@
+package ru.startandroid.study_cards001;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class Level1 extends AppCompatActivity implements View.OnClickListener {
+
+
+    public static int qt = 3; //кол-во карточек (кнопок / строк)
+    public static String [] question = new String[]{"lol","kek","bleat"}; //вводим массив  из  n строк
+    public static String [] avswer = new String[]{"lool","keek","bleeat"};
+    public static int number;
+    LinearLayout llMain;
+    Button [] btnNew = new Button[qt];//масив кнопок
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.base_activity_1);
+        llMain = (LinearLayout) findViewById(R.id.llMain);
+        for (int i = 0; i < btnNew.length; i++) {
+            btnNew[i] = new Button(this);
+            btnNew[i].setText(question[i]);
+            btnNew[i].setId(R.id.card);
+            llMain.addView(btnNew[i]);
+            btnNew[i].setOnClickListener(this);
+        }
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        int i = 0;
+        while (i < qt){
+           if (v == btnNew[i]){
+               number = i;
+               break;
+           }
+           else i++;
+        }
+        switch (v.getId()) {
+            case R.id.card:
+                Intent intent = new Intent(this, CheckQuesten.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+    }
+}
+
---
+++ study_cards004/src/main/java/ru/startandroid/study_cards001/Level7.java
@@ -0,0 +1,56 @@
+package ru.startandroid.study_cards001;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class Level7 extends AppCompatActivity implements View.OnClickListener {
+
+
+    int qt = 3; //кол-во карточек (кнопок / строк)
+    public static String [] question = new String[]{"lel","kek", "blead"}; //вводим массив  из  n строк
+    public static String [] avswer = new String[]{"leel","keek", "bleead"};
+    public static int number;
+    LinearLayout llMain;
+    Button [] btnNew = new Button[qt];//масив кнопок
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.base_activity_7);
+        llMain = (LinearLayout) findViewById(R.id.llMain);
+        for (int i = 0; i < btnNew.length; i++) {
+            btnNew[i] = new Button(this);
+            btnNew[i].setText(question[i]);
+            btnNew[i].setId(R.id.card);
+            llMain.addView(btnNew[i]);
+            btnNew[i].setOnClickListener(this);
+        }
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        int i = 0;
+        while (i < qt){
+            if (v == btnNew[i]){
+                number = i;
+                break;
+            }
+            else i++;
+        }
+        switch (v.getId()) {
+            case R.id.card:
+                Intent intent = new Intent(this, CheckQuesten.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+    }
+}
+
