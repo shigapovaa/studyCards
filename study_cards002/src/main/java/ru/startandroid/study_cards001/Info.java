36109eab39cbd52eaab77003afd85e31d18cbcb9
Mark Zhukov <MarkZhukov@bitbucket.org>
2016-11-20 03:11:42 +0300

    moore chenges (level1, checkquesten, checkanswer)

---
+++ study_cards002/src/main/java/ru/startandroid/study_cards001/Info.java
@@ -0,0 +1,15 @@
+package ru.startandroid.study_cards001;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+public class Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+    }
+}
